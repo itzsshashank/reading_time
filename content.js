a355090764e7a1631fbc9c5d67daa47e7820fed7
Shashank Shukla <121896399+itzsshashank@users.noreply.github.com>
2024-12-14 00:25:06 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,15 +1,14 @@
 const article = document.querySelector("article");
 
-// `document.querySelector` may return null if the selector doesn't match anything.
+// here `document.querySelector` may return null if the selector doesn't match anything.
 if (article) {
   const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const wordMatchRegExp = /[^\s]+/g; // regex
   const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
   const wordCount = [...words].length;
   const readingTime = Math.round(wordCount / 200);
   const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
+  // Using the same styling as the publish information in an article's header
   badge.classList.add("color-secondary-text", "type--caption");
   badge.textContent = `⏱️ ${readingTime} min read`;
 
